42091cf83c4772000f29cf0253928a991c7ebb44
yxyMissAll <1475425924@qq.com>
2021-06-18 00:13:03 +0800

    1.添加实体类

---
+++ his-commons/src/main/java/com/qidaiai/hiscommons/domain/BaseEntity.java
@@ -0,0 +1,13 @@
+package com.qidaiai.hiscommons.domain;
+
+import java.io.Serializable;
+
+/**
+ * 实体类的基础父类
+ * @author qidaiai
+ * @date 2021/06/17
+ */
+public class BaseEntity implements Serializable {
+
+    private static final long serialVersionUID=1L;
+}
---
+++ his-commons/src/main/java/com/qidaiai/hiscommons/domain/SimpalUser.java
@@ -0,0 +1,22 @@
+package com.qidaiai.hiscommons.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * 用户对象传输类
+ * @author qidaiai
+ * @date 2021/06/17
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SimpalUser implements Serializable {
+
+    private Serializable userId;
+
+    private String userName;
+}
