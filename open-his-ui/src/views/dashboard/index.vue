1de4c0600d083a10401555622cf4df5f273b598f
yxyMissAll <1475425924@qq.com>
2021-06-19 21:58:04 +0800

    1.完整前端页面提交；

---
+++ open-his-ui/src/views/dashboard/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="dashboard-container">
+    <div class="bg" />
+  </div>
+</template>
+<style  scoped>
+  .bg {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: url("../../assets/images/dashboard.jpg") no-repeat;
+    background-size: cover;
+  }
+</style>
